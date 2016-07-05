a40fc19428bc8ed784f4d5e9a2e80505ee731ddc
VioreliaVelea <viorelia.velea@techromix.com>
2016-07-05 17:47:06 +0300

    Added Ninject IOC Container

---
+++ MemorableTime/Infrastructure/NinjectBindings.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Ninject.Modules;
+
+namespace MemorableTime.Infrastructure
+{
+    public class NinjectBindings: NinjectModule
+    {
+        public override void Load()
+        {
+            //Bind<Interface>().To<Class>()
+        }
+    }
+}
